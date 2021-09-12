de631465bcd0c9d07ca53b6372c11fd7675b9b4b
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-12 11:12:23 +0300

    Final Commit

--- pandasMergingSortingAndConcatenating.py
+++ pandasMergingSortingAndConcatenating.py
@@ -26,3 +26,52 @@ print(df2)
 print("")
 print(df3)
 print("")
+
+print("")
+print("CONCATENATING")
+concatData = pd.concat([df1, df2, df3])
+print(concatData)
+print("")
+print("Concatenating on axis=1")
+concatData = pd.concat([df1, df2, df3], axis=1)
+print(concatData)
+
+left = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                     'A': ['A0', 'A1', 'A2', 'A3'],
+                     'B': ['B0', 'B1', 'B2', 'B3']})
+
+right = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                      'C': ['C0', 'C1', 'C2', 'C3'],
+                      'D': ['D0', 'D1', 'D2', 'D3']})
+
+print("")
+print("")
+print("MERGING")
+print(left)
+print("")
+print(right)
+print("")
+mergedData = pd.merge(left, right, how='inner', on='key')
+print(mergedData)
+print("")
+print("")
+print("JOINING")
+left = pd.DataFrame({'A': ['A0', 'A1', 'A2'],
+                     'B': ['B0', 'B1', 'B2']},
+                      index=['K0', 'K1', 'K2'])
+
+right = pd.DataFrame({'C': ['C0', 'C2', 'C3'],
+                    'D': ['D0', 'D2', 'D3']},
+                      index=['K0', 'K2', 'K3'])
+print(left)
+print("")
+print(right)
+print("")
+print(left.join(right))
+print("")
+print("INNER JOIN")
+print(left.join(right, how="inner"))
+print("")
+print("OUTER JOIN")
+print(left.join(right, how="outer"))
+print("")
